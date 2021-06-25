f769d6c2e800d8dfd57701c286c9810c0dc741ac
Dainis <moisejenko11@gmail.com>
2021-06-25 14:22:15 +0200

    Started work on About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,159 @@
-import React from "react";
+import React, { Suspense } from "react";
+
+import { TextureLoader } from "three";
+import { Canvas, useLoader, extend } from "@react-three/fiber";
+import { Effects, Html } from "@react-three/drei";
+
+import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faBasketballBall,
+	faCar,
+	faFish,
+	faGamepad,
+	faHandSpock,
+	faRoute,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { FormattedMessage, injectIntl } from "react-intl";
+
+import myImage from "../assets/images/myImage.jpg";
+
+import "../styles/About.css";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+extend({ GlitchPass });
+
+function Image() {
+	const texture = useLoader(TextureLoader, myImage);
+	return (
+		<mesh>
+			<planeBufferGeometry attach="geometry" args={[10, 10]} />
+			<meshBasicMaterial attach="material" map={texture} />
+		</mesh>
+	);
+}
 
 const About = () => {
 	return (
-		<div>
-			<h2>About me</h2>
-			<p>Hi, im Dainis</p>
+		<div className="container-fluid main">
+			<div className="row firstRow">
+				<div className="col-lg-4">
+					<Canvas>
+						<Effects>
+							<glitchPass attachArray="passes" />
+						</Effects>
+						<Suspense
+							fallback={
+								<Html center className="loading" children="Loading..." />
+							}
+						>
+							<Image />
+						</Suspense>
+					</Canvas>
+				</div>
+				<div className="col-lg-8">
+					<h1>
+						Hi <FontAwesomeIcon icon={faHandSpock} color={"darkcyan"} />,
+						<FormattedMessage id="AboutPage.welcome" />
+					</h1>
+					<p>
+						<FormattedMessage id="AboutPage.section1.txt1" />
+					</p>
+					<p>
+						<FormattedMessage id="AboutPage.section1.txt2" />
+					</p>
+					<p>
+						<FormattedMessage id="AboutPage.section1.txt3" />
+					</p>
+				</div>
+			</div>
+			<div className="hobbiesDiv">
+				<h3>
+					<FormattedMessage id="AboutPage.section2.header" />
+				</h3>
+				<div className="row">
+					<p>
+						<FormattedMessage id="AboutPage.section2.txt1" />
+					</p>
+					<span>
+						<FontAwesomeIcon icon={faGamepad} size={"2x"} />
+					</span>
+				</div>
+				<div className="row">
+					<p>
+						<FormattedMessage id="AboutPage.section2.txt2" />
+					</p>
+					<span>
+						<FontAwesomeIcon icon={faCar} size={"2x"} />
+					</span>
+				</div>
+				<div className="row">
+					<p>
+						<FormattedMessage id="AboutPage.section2.txt3" />
+					</p>
+					<span>
+						<FontAwesomeIcon icon={faRoute} size={"2x"} />
+					</span>
+				</div>
+				<div className="row">
+					<p>
+						<FormattedMessage id="AboutPage.section2.txt4" />
+					</p>
+					<span>
+						<FontAwesomeIcon icon={faFish} size={"2x"} />
+					</span>
+				</div>
+				<div className="row">
+					<p>
+						<FormattedMessage id="AboutPage.section2.txt5" />
+					</p>
+					<span>
+						<FontAwesomeIcon icon={faBasketballBall} size={"2x"} />
+					</span>
+				</div>
+			</div>
+			<div className="profSkillsDiv">
+				<h3>
+					<FormattedMessage id="AboutPage.section3.header" />
+				</h3>
+			</div>
+			<div className="otherSkillsDiv">
+				<h3>
+					<FormattedMessage id="AboutPage.section4.header" />
+				</h3>
+			</div>
+			<div className="contactDiv">
+				<h3>
+					<FormattedMessage id="AboutPage.section5.header" />
+				</h3>
+				<div>
+					<p className="row">
+						<FormattedMessage
+							id="AboutPage.section5.txt1"
+							values={{
+								b: (...chunks) => (
+									<b>
+										<i>{chunks}</i>
+									</b>
+								),
+							}}
+						/>
+					</p>
+					<span className="row">
+						<p>
+							<FormattedMessage id="AboutPage.section5.txt2" />
+						</p>
+						<a href="https://github.com/DainisM" target="blank">
+							{" "}
+							<FontAwesomeIcon icon={faGithub} size={"2x"} />{" "}
+						</a>
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default About;
+export default injectIntl(About);
--- src/messages.js
+++ src/messages.js
@@ -9,6 +9,33 @@ export default {
 			header: "My Skills",
 			txt1: "* Use your mouse to click on chest and play around",
 		},
+		AboutPage: {
+			welcome: " my name is Dainis",
+			section1: {
+				txt1: "I am a Data Technician specialising in programming student from Copenhagen, currently studying at TEC Ballerup.",
+				txt2: "I am 23 years old guy from Latvia who loves to learn and write code and experiment with a lot of new exciting things.",
+				txt3: "I always had a huge interest in technology starting from the 90´s game consoles, as a kid, to computers and other forms of technology because I think technologies helps us in our daily lives, while we work or relax, and I think technology is our future.",
+			},
+			section2: {
+				header: "My hobbies",
+				txt1: "Sometimes in my spare time I play PC games",
+				txt2: "I like cars & motorcycles - motorsport, tuning and other shenanigans",
+				txt3: "I love to travel {car road trip included}",
+				txt4: "I like to relax while I am fishing",
+				txt5: "Basketball is also one of my hobbies",
+			},
+			section3: {
+				header: "My professional skills",
+			},
+			section4: {
+				header: "My personal qualifications",
+			},
+			section5: {
+				header: "Contact me",
+				txt1: "You can reach me by email: <b>moisejenko11@gmail.com</b>",
+				txt2: "Or you can check out my github: ",
+			},
+		},
 	},
 	"da-DK": {
 		Header: {
@@ -19,6 +46,33 @@ export default {
 		HomePage: {
 			header: "Mine færdigheder",
 			txt1: "* Brug musen til at klikke på kisten og lege rundt ",
+		},
+		AboutPage: {
+			welcome: " mit navn er Dainis",
+			section1: {
+				txt1: "Jeg er Datatekniker med speciale i programmering student fra København som studerer i øjeblikket på TEC Ballerup.",
+				txt2: "Jeg er 23 år gammel fyr fra Letland, der elsker at lære og skrive kode samt eksperimentere med en masse nye spændende ting.",
+				txt3: "Jeg havde altid en enorm interesse for teknologi startende fra 90'ernes spilkonsoller, som barn, til computere og andre former for teknologi, fordi jeg tror, at teknologier hjælper os i vores daglige liv, mens vi arbejder eller slapper af, og jeg tror, teknologien er vores fremtid.",
+			},
+			section2: {
+				header: "Mine hobbyer",
+				txt1: "Nogle gange i min fritid spiller jeg pc-spil ",
+				txt2: "Jeg kan godt lide biler og motorcykler - motorsport, tuning og andre shenanigans ",
+				txt3: "Jeg elsker at rejse (bil road trip inkluderet) ",
+				txt4: "Jeg kan godt lide at slappe af mens jeg fisker ",
+				txt5: "Basketball er også en af mine hobbyer ",
+			},
+			section3: {
+				header: "Mine faglige kvalifikationer",
+			},
+			section4: {
+				header: "Mine personlige kvalifikationer",
+			},
+			section5: {
+				header: "Kontakt mig",
+				txt1: "Du kan nå mig via e-mail : <b>moisejenko11@gmail.com</b>",
+				txt2: "Eller du kan tjekke min github: ",
+			},
 		},
 	},
 };
